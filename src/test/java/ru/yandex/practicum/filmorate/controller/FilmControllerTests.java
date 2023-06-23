ebd8f3135dc6d2b76aee6725f15f2e344cd8faa1
Denis <dapolo84@yandex.ru>
2023-06-23 23:31:23 +1000

    feat: добавить базу данных (ТЗ-11)

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTests.java
@@ -1,4 +1,4 @@
-﻿package ru.yandex.practicum.filmorate.controller;
+package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
